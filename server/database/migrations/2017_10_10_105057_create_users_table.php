65f0003e853554bc873e00747e985d18e95a33b5
ycscholes <ycscholes@hotmail.com>
2017-10-10 17:35:23 +0800

    Auth

--- server/database/migrations/2017_10_10_105057_create_users_table.php
+++ server/database/migrations/2017_10_10_105057_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password', 60);
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -26,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::drop('users');
     }
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -7,10 +7,18 @@ $api->version('v1', [
 ], function ($api) {
     $api->post('/auth/login', [
         'as' => 'api.auth.login',
-        'uses' => 'AuthController@login',
+        'uses' => 'AuthController@login'
     ]);
-    $api->get('/', function () {
-        $a = 1;
-        echo $a;
+    $api->group([
+        'middleware' => 'api.auth',
+    ], function ($api) {
+        $api->get('/', [
+            'uses' => 'User@getUser',
+            'as' => 'api.index'
+        ]);
+        $api->get('/user', [
+            'as' => 'api.auth.user',
+            'uses' => 'AuthController@getUser'
+        ]);
     });
 });
